2f72e9a217832c332a83561654ef9f777cd4f9ee
owenlung <oleung89@gmail.com>
2026-04-07 17:53:12 -0400

    commit 2

--- web/firebase/auth.js
+++ web/firebase/auth.js
@@ -1,8 +1,8 @@
-import { auth } from "./firebaseConfig";
-import { CreateUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "./firebase";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword } from "firebase/auth";
 
 export const doCreateUserWithEmailAndPassword = async (email, password) => {
-    return CreateUserWithEmailAndPassword(auth, email, password);
+    return createUserWithEmailAndPassword(auth, email, password);
 };
 
 export const doSignInWithEmailAndPassword = async (email, password) => {
