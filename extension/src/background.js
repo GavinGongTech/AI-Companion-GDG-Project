26ee9e0eef6352bffc2ce4382968453f71a34996
clairewwwwww <clairepjwu@gmail.com>
2026-04-06 20:06:18 -0400

    finished setting up chrome extension

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onInstalled.addListener(() => {
   void chrome.sidePanel.setOptions({
-    path: "sidepanel.html",
+    path: "dist/sidepanel.html",
     enabled: true,
   });
 });
@@ -14,7 +14,6 @@ chrome.action.onClicked.addListener((tab) => {
 // Listen for messages from content script
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === "INGEST_PAGE") {
-    // Forward to side panel or handle directly
     sendResponse({ ok: true });
   }
   return true;
