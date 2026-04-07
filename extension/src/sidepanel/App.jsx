7a490cc77c572dca1da77f6485b601a0845ce94b
clairewwwwww <clairepjwu@gmail.com>
2026-04-06 21:03:05 -0400

    basic layout

--- extension/src/sidepanel/App.jsx
+++ extension/src/sidepanel/App.jsx
@@ -1,7 +1,23 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Shell } from "./Shell";
+import { Hub } from "./pages/Hub";
+import { Ask } from "./pages/Ask";
+import { Quiz } from "./pages/Quiz";
+import { Graph } from "./pages/Graph";
+import { Course } from "./pages/Course";
+
 export function App() {
   return (
-    <div style={{ padding: "20px", background: "white", color: "black", minHeight: "100vh" }}>
-      HELLO
-    </div>
+    <Shell>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/home" element={<Hub />} />
+        <Route path="/ask" element={<Ask />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/graph" element={<Graph />} />
+        <Route path="/course" element={<Course />} />
+        <Route path="*" element={<Navigate to="/home" replace />} />
+      </Routes>
+    </Shell>
   );
 }
